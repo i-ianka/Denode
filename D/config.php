056dc37e6e43f6def9baeee7e2cb9b7cfd1e32ec
i-ianka <inglidianka@gmail.com>
2017-08-20 20:44:11 -0300

    Update config.php

--- D/config.php
+++ D/config.php
@@ -1,6 +1,8 @@
 <?php
+date_default_timezone_set('America/Sao_Paulo');
 $host = "localhost";
 $usuario = "root";
 $senha = "";
 $banco = "0002050";
-
+$localtime = localtime();
+$localtime_assoc = localtime(time(), true);
